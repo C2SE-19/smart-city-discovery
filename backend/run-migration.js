0f79414813b4bf84c4b05930345ebd936ec6c0e4
. LOKKKNEEE <nguyenhuuloc5112004@gmail.com>
2026-03-16 14:20:05 +0700

    update profile

--- backend/run-migration.js
+++ backend/run-migration.js
@@ -15,7 +15,8 @@ async function runMigration() {
   try {
     const migrationFiles = [
       '20260313_create_users_table.sql',
-      '20260316_add_admin_role_and_seed_admin.sql'
+      '20260316_add_admin_role_and_seed_admin.sql',
+      '20260316_add_user_profile_fields.sql'
     ];
 
     for (const migrationFile of migrationFiles) {
