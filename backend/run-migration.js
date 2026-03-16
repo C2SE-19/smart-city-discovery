f1c9abdb364401450d31a131b534b93d76e302a3
. LOKKKNEEE <nguyenhuuloc5112004@gmail.com>
2026-03-17 00:14:50 +0700

    new code

--- backend/run-migration.js
+++ backend/run-migration.js
@@ -21,6 +21,7 @@ async function runMigration() {
       '20260316_add_admin_role_and_seed_admin.sql',
       '20260316_add_admin_role_and_seed_admin.sql',
       '20260316_add_user_profile_fields.sql',
+      '20260317_cleanup_nan_profile_fields.sql',
       '20260320_create_feedbacks_table.sql',
       '20260321_create_user_favorites.sql'
     ];
