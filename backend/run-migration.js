99edabbdba7b49454ecf6f01920027bc851ce9fe
vokhoa2505 <vovanhkhoa1@dtu.edu.vn>
2026-03-18 22:23:13 +0700

    update map

--- backend/run-migration.js
+++ backend/run-migration.js
@@ -24,7 +24,8 @@ async function runMigration() {
       '20260317_cleanup_nan_profile_fields.sql',
       '20260320_create_feedbacks_table.sql',
       '20260321_create_user_favorites.sql',
-      '20260322_create_merchant_services.sql'
+      '20260322_create_merchant_services.sql',
+      '20260318_enable_public_rls_baseline.sql'
     ];
 
     for (const migrationFile of migrationFiles) {
