707aebab6ec2f801659ba22f6188957b250edbac
. LOKKKNEEE <nguyenhuuloc5112004@gmail.com>
2026-03-27 09:17:22 +0700

    new 1.2

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const { supabaseAdmin } = require('./src/lib/supabase');
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '5mb' }));
+app.use(express.json({ limit: '10mb' }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Initialize Google OAuth2 Client
