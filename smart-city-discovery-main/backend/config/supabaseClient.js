9c974e7b7057c257428a923006f8d974ccf23b51
Văn Đức <ductruong22102004@gmail.com>
2026-04-03 10:42:38 +0700

    save current work

--- smart-city-discovery-main/backend/config/supabaseClient.js
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://ywgmkbxibgognsuzxacd.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3Z21rYnhpYmdvZ25zdXp4YWNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxODA3MzYsImV4cCI6MjA4ODc1NjczNn0.vEvjHyZG1dHoGhfhYy2vzToyddMhpzHLErLZffR8UsQ'
-
-const supabase = createClient(supabaseUrl, supabaseKey)
-
-export default supabase
